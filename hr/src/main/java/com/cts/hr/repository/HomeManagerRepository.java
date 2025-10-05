8ad6b4e132059ea0bcc9f1c357922439b895c60a
shubham dubey <shubhamd1109@gmai.com>
2025-10-05 19:11:45 +0530

    Removed home manager entity, repository and dto.

--- hr/src/main/java/com/cts/hr/repository/HomeManagerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cts.hr.repository;
-
-import com.cts.hr.entity.HomeManager;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HomeManagerRepository extends CrudRepository<HomeManager,Integer> {
-}
